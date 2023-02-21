0520187b1556160beb62e031e47f5e75c371159b
Kat_Chudy <kec21@fsu.edu>
2023-02-21 18:01:57 -0500

    trying to figure this out

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,56 @@
 #include <SPI.h>
 #include <MFRC522.h>
- 
+
 #define SS_PIN 10
 #define RST_PIN 9
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
- 
-void setup() 
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
+boolean successfullyReadtheFirstCard = false;
+
+void setup()
 {
-  Serial.begin(9600);   // Initiate a serial communication
-  SPI.begin();      // Initiate  SPI bus
-  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.begin(9600); // Initiate a serial communication
+  SPI.begin();        // Initiate  SPI bus
+  mfrc522.PCD_Init(); // Initiate MFRC522
   Serial.println("Approximate your card to the reader...");
   Serial.println();
-
 }
-void loop() 
+void loop()
 {
   // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  if (!mfrc522.PICC_IsNewCardPresent())
   {
     return;
   }
   // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  if (!mfrc522.PICC_ReadCardSerial())
   {
     return;
   }
-  //Show UID on serial monitor
+  // Show UID on serial monitor
   Serial.print("UID tag :");
-  String content= "";
+  String content = "";
   byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  for (byte i = 0; i < mfrc522.uid.size; i++)
   {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   Serial.println();
   Serial.print("Message : ");
   content.toUpperCase();
-  if (content.substring(1) == "91 D4 35 1D", "A1 21 13 1D") //change here the UID of the card/cards that you want to give access
+  if (content.substring(1) == "91 D4 35 1D")
   {
-    Serial.println("Authorized access");
-    Serial.println();
-    delay(3000);
+    successfullyReadtheFirstCard = true;
   }
- 
- else   {
-    Serial.println(" Access denied");
-    delay(3000);
+    if (content.substring(1) == "48 23 D8 10")
+  {
+    successfullyReadtheFirstCard = true;
+    Serial.println("Correct Order!");
   }
-} 
+  else
+  {
+    Serial.println("Wrong!");
+  }
+}
